8a475976e7b5b119fe6788fc1b70b2fbf19cf9a5
SHASHANK KANOJIA <115026120+ShashankKan0jia@users.noreply.github.com>
2026-09-25 19:47:42 +0530

    Normalize student model fields

--- backend/models/Student.js
+++ backend/models/Student.js
@@ -5,18 +5,22 @@ const studentSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
+    trim: true,
   },
   name: {
     type: String,
     required: true,
+    trim: true,
   },
   class: {
     type: String,
     required: true,
+    trim: true,
   },
   school: {
     type: String,
     required: true,
+    trim: true,
   },
 });
 
